f9a86f156f87b1162815fdb28e5825ee11158579
Rodrigo Weber <rodrigoweber97@hotmail.com>
2022-09-13 19:22:01 -0300

    perfil de teste criado

--- backend/src/main/java/com/webertech/helpdesk/HelpdeskApplication.java
+++ backend/src/main/java/com/webertech/helpdesk/HelpdeskApplication.java
@@ -17,30 +17,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.Arrays;
 
 @SpringBootApplication
-public class HelpdeskApplication implements CommandLineRunner {
+public class HelpdeskApplication {
 
-	@Autowired
-	private TecnicoRepository tecnicoRepository;
-	@Autowired
-	private ClienteRepository clienteRepository;
-	@Autowired
-	private ChamadoRepository chamadoRepository;
+    public static void main(String[] args) {
+        SpringApplication.run(HelpdeskApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelpdeskApplication.class, args);
-	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		Tecnico tec1 = new Tecnico(null, "Valdir Cesar", "1234567834", "valdir@email.com", "123");
-		tec1.addPerfil(Perfil.ADMIN);
-
-		Cliente cli1 = new Cliente(null, "Linus Torvalds", "00988523413", "torvalds@email.com", "123");
-
-		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro Chamado", tec1, cli1);
-
-		tecnicoRepository.saveAll(Arrays.asList(tec1));
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		chamadoRepository.saveAll(Arrays.asList(c1));
-	}
 }
---
+++ backend/src/main/java/com/webertech/helpdesk/services/DBService.java
@@ -0,0 +1,39 @@
+package com.webertech.helpdesk.services;
+
+import com.webertech.helpdesk.domain.Chamado;
+import com.webertech.helpdesk.domain.Cliente;
+import com.webertech.helpdesk.domain.Tecnico;
+import com.webertech.helpdesk.domain.enums.Perfil;
+import com.webertech.helpdesk.domain.enums.Prioridade;
+import com.webertech.helpdesk.domain.enums.Status;
+import com.webertech.helpdesk.repositories.ChamadoRepository;
+import com.webertech.helpdesk.repositories.ClienteRepository;
+import com.webertech.helpdesk.repositories.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class DBService {
+
+    @Autowired
+    private TecnicoRepository tecnicoRepository;
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Autowired
+    private ChamadoRepository chamadoRepository;
+
+    public void instanciaDB() {
+        Tecnico tec1 = new Tecnico(null, "Valdir Cesar", "1234567834", "valdir@email.com", "123");
+        tec1.addPerfil(Perfil.ADMIN);
+
+        Cliente cli1 = new Cliente(null, "Linus Torvalds", "00988523413", "torvalds@email.com", "123");
+
+        Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro Chamado", tec1, cli1);
+
+        tecnicoRepository.saveAll(Arrays.asList(tec1));
+        clienteRepository.saveAll(Arrays.asList(cli1));
+        chamadoRepository.saveAll(Arrays.asList(c1));
+    }
+}
