8f4cb5f43743ec21368e93b10c55679e9cef4791
Rodrigo Weber <rodrigoweber97@hotmail.com>
2022-09-14 10:55:51 -0300

    padrao DTO para Tecnico

--- backend/src/main/java/com/webertech/helpdesk/resources/TecnicoResource.java
+++ backend/src/main/java/com/webertech/helpdesk/resources/TecnicoResource.java
@@ -1,6 +1,7 @@
 package com.webertech.helpdesk.resources;
 
 import com.webertech.helpdesk.domain.Tecnico;
+import com.webertech.helpdesk.domain.dtos.TecnicoDTO;
 import com.webertech.helpdesk.services.TecnicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,9 +19,9 @@ public class TecnicoResource {
     private TecnicoService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+    public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
         Tecnico obj = service.findById(id);
-        return ResponseEntity.ok().body(obj);
+        return ResponseEntity.ok().body(new TecnicoDTO(obj));
     }
 
 }
