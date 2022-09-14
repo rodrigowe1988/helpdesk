7030610ad29af7d7323ead3a0c9add4d03c1d825
Rodrigo Weber <rodrigoweber97@hotmail.com>
2022-09-14 15:17:09 -0300

    excecao para CPF e EMAIL ja cadastrados no banco de dados

--- backend/src/main/java/com/webertech/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/webertech/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.webertech.helpdesk.resources.exceptions;
 
+import com.webertech.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.webertech.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,5 +18,14 @@ public class ResourceExceptionHandler {
                 "Object Not Found", ex.getMessage(), request.getRequestURI());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex, HttpServletRequest request) {
+        StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+                "Violação de dados", ex.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
--- backend/src/main/java/com/webertech/helpdesk/services/TecnicoService.java
+++ backend/src/main/java/com/webertech/helpdesk/services/TecnicoService.java
@@ -1,8 +1,11 @@
 package com.webertech.helpdesk.services;
 
+import com.webertech.helpdesk.domain.Pessoa;
 import com.webertech.helpdesk.domain.Tecnico;
 import com.webertech.helpdesk.domain.dtos.TecnicoDTO;
+import com.webertech.helpdesk.repositories.PessoaRepository;
 import com.webertech.helpdesk.repositories.TecnicoRepository;
+import com.webertech.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.webertech.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +18,8 @@ public class TecnicoService {
 
     @Autowired
     private TecnicoRepository repository;
+    @Autowired
+    private PessoaRepository pessoaRepository;
 
     public Tecnico findById(Integer id) {
         Optional<Tecnico> obj = repository.findById(id);
@@ -27,7 +32,20 @@ public class TecnicoService {
 
     public Tecnico create(TecnicoDTO objDTO) {
         objDTO.setId(null);
+        validaPorCpfEEmail(objDTO);
         Tecnico newObj = new Tecnico(objDTO);
         return repository.save(newObj);
+    }
+
+    private void validaPorCpfEEmail(TecnicoDTO objDTO) {
+        Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+        if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+            throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");
+        }
+
+        obj = pessoaRepository.findByEmail(objDTO.getEmail());
+        if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+            throw new DataIntegrityViolationException("Email já cadastrado no sistema!");
+        }
     }
 }
