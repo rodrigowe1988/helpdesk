53723f3a857b45f5d61d88d82b059d3272dbd7e8
Rodrigo Weber <rodrigoweber97@hotmail.com>
2022-09-14 18:04:57 -0300

    delete para Tecnico implementado

--- backend/src/main/java/com/webertech/helpdesk/services/TecnicoService.java
+++ backend/src/main/java/com/webertech/helpdesk/services/TecnicoService.java
@@ -59,4 +59,12 @@ public class TecnicoService {
     }
 
 
+    public void delete(Integer id) {
+        Tecnico obj = findById(id);
+        if(obj.getChamados().size() > 0) {
+            throw new DataIntegrityViolationException("Técnico possui ordens de serviço e não pode ser deletado!");
+        }
+        repository.deleteById(id);
+
+    }
 }
