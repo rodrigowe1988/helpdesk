9cbda6edd1109a3d60e7142468c04a1a76a6817b
Rodrigo Weber <rodrigoweber97@hotmail.com>
2022-09-14 18:25:41 -0300

    CRUD para cliente implementado

--- backend/src/main/java/com/webertech/helpdesk/domain/Cliente.java
+++ backend/src/main/java/com/webertech/helpdesk/domain/Cliente.java
@@ -1,13 +1,14 @@
 package com.webertech.helpdesk.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.webertech.helpdesk.domain.dtos.ClienteDTO;
 import com.webertech.helpdesk.domain.enums.Perfil;
 
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 public class Cliente extends Pessoa{
@@ -26,6 +27,18 @@ public class Cliente extends Pessoa{
         super(id, nome, cpf, email, senha);
         addPerfil(Perfil.CLIENTE);
     }
+
+    public Cliente(ClienteDTO obj) {
+        super();
+        this.id = obj.getId();
+        this.nome = obj.getNome();
+        this.cpf = obj.getCpf();
+        this.email = obj.getEmail();
+        this.senha = obj.getSenha();
+        this.perfis = obj.getPerfis().stream().map(x-> x.getCodigo()).collect(Collectors.toSet());
+        this.dataCriacao = obj.getDataCriacao();
+    }
+
 
     public List<Chamado> getChamados() {
         return chamados;
