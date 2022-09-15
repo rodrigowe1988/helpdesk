9da4ca25525f08b423232f697ec1527270ec14a0
Rodrigo Weber <rodrigoweber97@hotmail.com>
2022-09-15 19:10:18 -0300

    rota para tecnico

--- frontend/src/app/components/nav/nav.component.ts
+++ frontend/src/app/components/nav/nav.component.ts
@@ -7,6 +7,8 @@ import { Router } from "@angular/router";
   styleUrls: ["./nav.component.css"],
 })
 export class NavComponent implements OnInit {
+  github: string = "https://github.com/rodrigowe1988/helpdesk";
+
   constructor(private router: Router) {}
 
   ngOnInit(): void {
